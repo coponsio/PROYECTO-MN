1a87385c50e7b8abc6e73624db529e5a64f3a04e
coponsio <115663827+coponsio@users.noreply.github.com>
2022-10-12 14:33:07 -0500

    primera subida

---
+++ base de preguntas.js
@@ -0,0 +1,18 @@
+let BaseDePreguntas=[
+{
+        pregunta: "encuentre el valor indicado",
+        consejo:"texto",
+imagen:"https://i.ibb.co/Jzxx1rq/Captura-de-pantalla-2022-10-12-003525.png",
+respuesta: "aqui la correcta",
+
+incorrectas:[ "incorrecta 1", "incorrecta 2",  "incorrecta 3", ]
+},
+
+{
+    pregunta: "encuentre el valor indicado",
+    consejo:"texto",
+imagen:"https://i.ibb.co/Jzxx1rq/Captura-de-pantalla-2022-10-12-003525.png",
+respuesta: "correcta",
+incorrectas:[" incorrecta 1", "incorrecta 2", "incorrecta 3",]
+}
+]
